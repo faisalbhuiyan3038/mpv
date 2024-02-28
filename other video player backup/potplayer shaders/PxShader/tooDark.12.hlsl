5e9a68d6b857c68ec6b821cb75d5fa31466b0eb2
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-02-29 02:46:08 +0600

    update potplayer shaders

--- other video player backup/potplayer shaders/PxShader/tooDark.12.hlsl
+++ other video player backup/potplayer shaders/PxShader/tooDark.12.hlsl
@@ -2,28 +2,27 @@
 #define Screen 0.12
 
 /* --- tooDark (dx9) small adjustment, ex: 0.12 --- */
-/* v1.41 (2023-11) released by butterw under GPLv3
-(1 texture, 3 arithmetic)
-These parameters provide a small brightness increase to boost a clip that could maybe be brighter.
+/* These parameters provides a small brightness increase to boost a clip that could maybe be brighter.
 If clipping highlights isn't an issue, alternatives would be: brightenD.03
 or for a brightness+contrast increase: exposure.04.
 ---
+v1.40 (2023-07) released by butterw under GPLv3
+ (1 texture, 3 arithmetic)
 
 Apply when the picture is too dark.
-Increases brightness without clipping (Black point and White point aren't changed).
+Increases brightness without clipping (Black point and White point are not changed).
 
 Photoshop Screen Blend, brightness/contrast curve:
-out = f(x, S: Screen) = -S*x*x + (1+S)*x = -S*x^2 +S*x +x
-    with x, out: pixel.rgb in [0, 1.0]
-    and Screen >=0
-f(x:0)=0; f(0.5)=0.5; f(1)=1.
+out = -Screen*x*x + (1+Screen)*x = -Screen*x^2 +Screen*x + x
+	with x, out: pixel.rgb in [0, 1.0]
+	and Screen >=0
 
 parameter Screen [-1, 1.0], 0: no effect.
 >> Positive: (Screen Blend) increases brightness, increases constrast in shadows, ex: 0.24
-    - Brightness increase is maximum at the mid-point (S/4).
-    - Contrast = 1+S -2*S*x
-    Contrast is maximum at the Black point (1+S) and decreases linearly with input level.
-    Contrast:1 at the midpoint.
+	- Brightness increase is maximum at the mid-point (Screen/4).
+	- Contrast = 1+Screen -2*Screen*x
+	Contrast is maximum at the Black point (1+Screen) and decreases linearly with input level.
+	Contrast:1 at the mid-point.
 
 negative: (Multiply Blend) decreases brightness, increases contrast in highlights.
 
@@ -32,8 +31,8 @@ sampler s0: register(s0);
 #define Multiply -Screen //to ensure a positive user parameter.
 
 float4 main(float2 tex: TEXCOORD0): COLOR {
-    float4 c0 = tex2D(s0, tex);
+	float4 c0 = tex2D(s0, tex);
 
-    c0.rgb = lerp(c0, c0*c0, Multiply).rgb;
-    return c0;
+	c0.rgb = lerp(c0, c0*c0, Multiply).rgb;
+	return c0;
 }
